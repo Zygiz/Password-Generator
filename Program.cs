824dc69d1694529c3f47c495b2e1e7998ec18085
Zygiz <zygimantas.mikaila@gmail.com>
2026-05-20 01:21:31 +0300

    1st stage prep

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Github testing");
+﻿//System.Security.Cryptography.RandomNumberGenerator;
+
+
+string lowercase = "abcdefghijklmnopqrstuvwxyz";
+string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string numbers = "0123456789";
+string specials = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
+
+//Console.WriteLine("Github testing");
+
+// 1 st stage: random 12 number sequence
