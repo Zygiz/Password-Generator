a292ae977a5020a8d5cbb6e7c08a71c6eb6cb5d6
Zygiz <zygimantas.mikaila@gmail.com>
2026-05-20 15:12:17 +0300

    Stage 1 completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿//System.Security.Cryptography.RandomNumberGenerator;
+﻿using System;
+using System.Text;
 
 
 string lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -9,3 +10,12 @@ string specials = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
 //Console.WriteLine("Github testing");
 
 // 1 st stage: random 12 number sequence
+StringBuilder password = new StringBuilder();
+Random random = new Random();
+char[] numbs = numbers.ToCharArray();
+for(int i = 0; i < 12; i++)
+{
+  // pick a random index within the char array and append the char
+  password.Append(numbs[random.Next(numbs.Length)]);
+}
+System.Console.WriteLine(password);
