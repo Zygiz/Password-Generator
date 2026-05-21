1bbb22ec5943056001d88f12141f0153264df1d8
Zygiz <zygimantas.mikaila@gmail.com>
2026-05-21 23:37:58 +0300

    Refined stage 1

--- Program.cs
+++ Program.cs
@@ -2,20 +2,49 @@
 using System.Text;
 
 
-string lowercase = "abcdefghijklmnopqrstuvwxyz";
-string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string numbers = "0123456789";
-string specials = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
-
-//Console.WriteLine("Github testing");
-
-// 1 st stage: random 12 number sequence
-StringBuilder password = new StringBuilder();
-Random random = new Random();
-char[] numbs = numbers.ToCharArray();
-for(int i = 0; i < 12; i++)
+internal class Program
 {
-  // pick a random index within the char array and append the char
-  password.Append(numbs[random.Next(numbs.Length)]);
+    private static void Main(string[] args)
+    {
+        string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string numbers = "0123456789";
+        string specials = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
+
+        //Console.WriteLine("Github testing");
+
+        // 1 st stage: random 12 number sequence
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+        char[] numbs = numbers.ToCharArray();
+
+        //Stage 1
+        //int length;
+        System.Console.WriteLine("=== Secure Password Generator ===\n");
+        System.Console.Write("Enter password length: ");
+
+        try
+        {
+            int length = int.Parse(System.Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                // pick a random index within the char array and append the char
+                password.Append(numbs[random.Next(numbs.Length)]);
+            }
+            System.Console.WriteLine(password);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: you must write int numbers!\n");
+        }
+        catch (OverflowException)
+        {
+            System.Console.WriteLine("Error: number is too big!\n");
+        }
+        finally
+        {
+            System.Console.WriteLine("=================================");
+        }
+        
+    }
 }
-System.Console.WriteLine(password);
